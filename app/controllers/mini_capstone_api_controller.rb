69bf7ad7cabe10a99f90a7894ed722d613e7dc39
Collin Plank <collin.plank@gmail.com>
2024-11-06 14:15:38 -0600

    New route for one product

--- app/controllers/mini_capstone_api_controller.rb
+++
@@ -1,7 +0,0 @@
-class MiniCapstoneApiController < ApplicationController
-end
-
-def apple
-  @product = Product.first
-  render template: "contracts/show"
-end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   # root "posts#index
 
   get "/all_products", controller: "products", action: "all_products"
+  get "/one_product/:id", controller: "products", action: "one_product"
 end
