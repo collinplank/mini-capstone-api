9d72dba9a0a48d9c65ff9f469d70084ffd97fb0e
Collin Plank <collin.plank@gmail.com>
2024-11-06 11:59:28 -0600

    Initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+product = Product.create(name: "Apple", price: 1.99, image_url: " ", description: "Red fruit")
+product = Product.create(name: "Banana", price: 2.99, image_url: " ", description: "Yellow fruit")
+product = Product.create(name: "Blueberry", price: 3.99, image_url: " ", description: "Berry that is blue")
+product = Product.create(name: "Pear", price: 4.99, image_url: " ", description: "Kinda yellowish")
+product = Product.create(name: "Plum", price: 5.99, image_url: " ", description: "Purple fruit")
