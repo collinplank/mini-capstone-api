7d7a63262212bb71139a95cdccc2972f00044ea9
Collin Plank <collin.plank@gmail.com>
2024-12-13 14:26:01 -0600

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com", "localhost:5173", "<mini-capstone-api-7hv2.onrender.com>"
+    origins "example.com", "localhost:5173", "<mini-capstone-frontend-c6u2.onrender.com>"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
